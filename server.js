aef2be895d3957c7eaf1572888b4cd77f20adfe5
Muhammad Akhsanul Zidan <akhsanul.zidan25@gmail.com>
2024-08-22 14:58:49 +0700

    updatess

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/styles.css', (req, res) => {
     res.sendFile(path.join(__dirname, 'styles.css'));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
